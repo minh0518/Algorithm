ea28bd2c13f62d6d9010c6e652b9144685eb3fc3
minh0518 <rubics@naver.com>
2021-12-30 01:09:33 +0900

    21.12.30

--- 2675.js
+++ 2675.js
@@ -9,20 +9,19 @@ const rl = readline.createInterface({
 const data=[]
 
 rl.on('line', (input) => {
-    data.push(input);
+    data.push(input)
  }).on('close',()=>{ 
 
     let T= +data[0]
-    let R
-    let S
-    let result='';
+
+    let result=''
     for(let i=1; i<=T; i++){
-        R= +data[i].split(' ')[0];
-        S=data[i].split(' ')[1];    
+        let R= +data[i].split(' ')[0]
+        let S=data[i].split(' ')[1]   
 
         for(let i=0; i<S.length; i++){
             for(let j=0; j<R; j++){
-                result+=S[i];
+                result+=S[i]
             }
         }
         console.log(result)
--- test.js
+++ test.js
@@ -6,72 +6,41 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let data;
-let max=0;
+const data=[]
+
 rl.on('line', (input) => {
-    data=input.toUpperCase();
- });
-rl.on('close',()=>{
-   let count=new Array(26).fill(0);
+    data.push(input)
+ }).on('close',()=>{ 
 
-   console.log(data);
-   for(let i=0; i<data.length; i++){
-     count[((data[i].charCodeAt())-65)]++;
-   }
+  let count=Number(data[0])
+  let result=0;
 
-  for(let i=0; i<count.length; i++){
-     
-    console.log(count[i]);
-      
+
+
+  for(let i=1; i<=count; i++){
+      let word=data[i]
+      let status=true;
+      let letter=[]
+      for(let j=0; j<word.length; j++){
+        
+       if(letter.indexOf(word[j]) == -1){
+         letter.push(word[j])
+       }
+       else{
+         if(letter.indexOf(word[j]) != (letter.length)-1 ){
+           status=false
+           break;
+         } 
+        }
+
+      }
+
+      if(status){
+        result++;
+      }
   }
 
-  
-//65부터 90
+  console.log(result);
+
   process.exit();
 })
-
-
-//이전에도 필기했다시피  data=input 이렇게만 받아도 자동으로
-//입력값은 문자열로 들어온다.
-
-
-
-// data=input; 로 입력받으면 aaabc로 입력받아지고 (문자열)
-
-// data=inupt.split(" "); 으로 입력받으면 ['aaabc']가 된다.
-
-
-
-
-//극도로 민감한 사향이다.
-//문자열은 []로 접근이 가능하다
-//이게 문자열이 배열이라던가 객체라던가 이런게 아니라 (문자열은 프리미티브이다)
-//그리고 JS에서 문자열은 객체가 아니라
-//Javascript에서 문자열은 Array Like 형태로 참조할 수 있기 때문이다.
-
-
-//그리고 String객체 (이건 원시값이랑 다른 진짜 말 그대로 new String 객체)
-//의 메소드나 프로퍼티 (touppercase나 indexof같은 것들)를  ="" 같은
-//문자열 리터럴로 선언한 원시값이 사용할 수 있는 것은 
-//메소드나 프로퍼티를 사용하는 순간 래퍼객체가 일시적으로 생성이 되어서
-//지원을 해 주고 실행이 되고 난 다음엔 래퍼객체가 사라지기 때문이다.
-
-
-//new String 과 String은 다르다
-//전자는 생성자를 사용한 '객체 생성'이고 후자는 단지 형변환의 기능만 수행한다
-//그리고 JS튜토리얼에는 new String이라는 것 자체를 안쓴다
-//왜냐하면 웬만하면 JS에는 문자열 리터럴을 통해 원시값으로 string을 사용해도
-//그 new String이 제공하는 메소드나 프로퍼티를 다 사용할 수 있기 때문이다.
-//다른 이유는 노션에 래퍼객체 필기한 것을 보자.
-
-
-//    alert(?typeof?'hello'?);? // String
-//alert(?typeof?new?String('hello')?);? //Object
-
-
-
-
-//java에선 문자열자체가 다 객체가 되는 것으로 배워서 헷갈리지만
-//(사실 java에서도 문자열 리터럴과 new String은 서로 저장되는 주소값은
-//다르긴 합니다)
-//jS에선 분명히 다릅니다.
