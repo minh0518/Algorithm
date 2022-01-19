e4c2172d891d6bc64767a5dfd18e5c69edf7a058
minh0518 <rubics@naver.com>
2022-01-19 15:16:59 +0900

    22.1.19

--- test.js
+++ test.js
@@ -12,29 +12,7 @@ rl.on('line', (input) => {
     data.push(input)  
  }).on('close',()=>{
 
-    let S=data[0]
-    let sum=0
-    let stringData=[]
-    let result
-    for(let i of S){
-      if(i.charCodeAt(0)>=65){
-        stringData.push(i)
-      }
-      else{
-          sum+=Number(i)
-      }
-      
-    }
-
-    result=(stringData.sort())
-
-    if(sum!=0){
-      result.push(sum)
-    }
-    
-    console.log(result.join(''))
-    //join으로 배열을 하나의 값으로 합침
-    
+    // readme파일 생성과 관련된 커밋충돌 유무 확인용
 
 
     process.exit();
