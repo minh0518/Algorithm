024f95c7b0582a9d1f56cbcaacc5135d673b1252
minh0518 <rubics@naver.com>
2022-06-29 13:52:22 +0900

    22.6.29

---
+++ JSstudyGroup/programmers/2019 KAKAO BLIND RECRUITMENT/실패율.js
@@ -0,0 +1,27 @@
+function solution(N, stages) {
+    let answer = []
+  
+    let result = []
+    for (let i = 0; i < N; i++) {
+      let length = stages.length
+  
+      stages = stages.filter((item) => {
+        return i + 1 < item
+      })
+      result.push([i + 1, (length - stages.length) / length])
+    }
+  
+    // console.log(result)
+  
+    result.sort((a, b) => {
+      return b[1] - a[1]
+    })
+  
+    for (let i of result) {
+      answer.push(i[0])
+    }
+  
+    // console.log(answer)
+  
+    return answer
+  }
--- test.js
+++ test.js
@@ -1,41 +1,27 @@
-function solution(rows, columns, queries) {
-  const arr = new Array(rows).fill().map(() => [])
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < columns; j++) {
-      arr[i][j] = j + 1 + i * columns
-    }
+function solution(N, stages) {
+  let answer = []
+
+  let result = []
+  for (let i = 0; i < N; i++) {
+    let length = stages.length
+
+    stages = stages.filter((item) => {
+      return i + 1 < item
+    })
+    result.push([i + 1, (length - stages.length) / length])
   }
 
-  const mins = []
+  // console.log(result)
 
-  queries.map((query) => {
-    const [x1, y1, x2, y2] = query.map((i) => i - 1)
-    let min = arr[x1][y1],
-      tmp = arr[x1][y1]
-
-    for (let i = x1; i < x2; i++) {
-      arr[i][y1] = arr[i + 1][y1]
-      min = Math.min(min, arr[i][y1])
-    }
-    for (let i = y1; i < y2; i++) {
-      arr[x2][i] = arr[x2][i + 1]
-      min = Math.min(min, arr[x2][i])
-    }
-    for (let i = x2; i > x1; i--) {
-      arr[i][y2] = arr[i - 1][y2]
-      min = Math.min(min, arr[i][y2])
-    }
-    for (let i = y2; i > y1; i--) {
-      arr[x1][i] = arr[x1][i - 1]
-      min = Math.min(min, arr[x1][i])
-    }
-    arr[x1][y1 + 1] = tmp
-
-    //console.log(arr)
-    mins.push(min)
+  result.sort((a, b) => {
+    return b[1] - a[1]
   })
 
-  return mins
-}
+  for (let i of result) {
+    answer.push(i[0])
+  }
 
-console.log(solution(6, 6, [[2, 2, 5, 4],[3, 3, 6, 6],[5, 1, 6, 3]]))
+  // console.log(answer)
+
+  return answer
+}
