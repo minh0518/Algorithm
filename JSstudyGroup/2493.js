9a0b061651d170d73445b130430a4ba70265c965
minh0518 <rubics@naver.com>
2022-12-15 00:18:34 +0900

    22.12.15

--- JSstudyGroup/2493.js
+++ JSstudyGroup/2493.js
@@ -1,84 +1,46 @@
-const readline = require('readline')
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-})
+});
 
-const data = []
+const data = [];
 
 rl.on('line', (input) => {
-  data.push(input)
+  data.push(input);
 }).on('close', () => {
+  let N = +data.shift();
 
-  let N=+data.shift()
+  let towers = data.shift().split(' ').map(Number);
 
-  let towers=data.shift().split(' ').map(Number)
+  let stack = [];
 
-  let stack=[]
+  let result = [0];
 
-  let answer=[]
+  for (let i = 1; i < towers.length; i++) {
+    stack.push([towers[i - 1], i]); //이전 값을 넣어줌
 
-  for(let i=0; i<N; i++){
-    const currentTower = {
-      index: i + 1,
-      height: towers[i]
+    while (stack.length) {
+      if (stack[stack.length - 1][0] < towers[i]) { 
+        
+        // 스택에 있는 것 보다 현재 타워가 크면 스택에서 pop
+        stack.pop();
+      } else {
+        break;
+      }
     }
 
+    if (!stack.length) result.push(0);
+    else {
 
-    if(!stack.length){ //스택이 빈 것은 앞에 탑이 하나도 없는 것이므로
-                      //스택에 현재 탑을 push하고 정답으로 0을 push
-      stack.push(currentTower)
-      answer.push(0)
-      continue
-    }
-
-    if(stack[stack.length-1].height<currentTower.height){
-        //스택의 맨 끝에 있는(=현재 탑과 가장 가까운곳에 있는 탑)탑의 높이보다
-        //현재 탑이 더 높다면 레이저신호를 수신하지 못한다
-        //그러므로 stack을 돌면서 찾아봐야한다
-
-      while(stack.length){ //스택이 빌 때까지
-        if(stack[stack.length-1].height>=currentTower.height){ 
-                                    //stack을 돌면서
-                                    //현재 입력받은 타워의 높이보다 큰 타워를 찾는다
-          break //찾으면 바로break 즉, 더이상 스택에서 탐색하지 않음
-        }
-        else{
-          stack.pop() //계속해서 스택을 탐색함
-                    //여기서 현재 탑보다 낮은 값은 어차피 레이저가 수신되지 않으므로
-                    //아예 스택에서 삭제시킨다
-        }
-      }
-
-      //while문을 탈출했다면 stack이 빌 때까지 다 돌았거나, 
-      //중간에 현재 타워보다 큰 값을 찾았다는 것이다
-      if(!stack.length){ //stack이 빌 때까지 다 돌았을 경우
-        answer.push(0) //정답으로 0 push
-      }
-      else{ //중간에 현재 타워보다 큰 값을 찾았을 경우
-        answer.push(stack[stack.length - 1].index)
-        //정답으로 스택에서 찾은 타워의 값 push
-      }
-
-      stack.push(currentTower)
-      //현재 타워를 스택에 push
-    }
-
-    else{ //스택의 맨 끝에 있는(=현재 탑과 가장 가까운곳에 있는 탑)
-          //탑의 높이가 현재 탑보다 더 높다면 그게 바로 정답이 되므로
-      answer.push(stack[stack.length - 1].index)
-      stack.push(currentTower)
-      //그리고 스택에 현재 탑 push
+      // 스택에 값이 남아 있다면 가장 최근에 push된 것을 정답배열에 push
+      result.push(stack[stack.length - 1][1]);
     }
   }
 
+  console.log(result.join(' '));
 
-
-  console.log(answer.join(' '))
-  
-
-  
-  process.exit()
-})
-
+  //6 9 5 7 4
+  process.exit();
+});
--- test.js
+++ test.js
@@ -10,75 +10,37 @@ const data = [];
 rl.on('line', (input) => {
   data.push(input);
 }).on('close', () => {
-  let [N, M] = data.shift().split(' ').map(Number);
+  let N = +data.shift();
 
-  let box = data.map((i) => i.split(' ').map(Number));
+  let towers = data.shift().split(' ').map(Number);
 
-  //상하좌우
-  let [dx, dy] = [
-    [-1, 1, 0, 0],
-    [0, 0, -1, 1],
-  ];
+  let stack = [];
 
-  //d가 현재 빛의 방향 direction이 변환기
-  let direction = [3, 2, 1, 0];
-  //상 하 좌 우 (상이 상에서 쏜다는게 아니라 상으로 가는 방향임)
-  //3 2 1 0
+  let result = [0];
 
-  const move = (d, x, y) => {
-    while (x >= 0 && x < N && y >= 0 && y < M) {
-      if (box[x][y] === 1) {
-        d = direction[d];
+  for (let i = 1; i < towers.length; i++) {
+    stack.push([towers[i - 1], i]); //이전 값을 넣어줌
+
+    while (stack.length) {
+      if (stack[stack.length - 1][0] < towers[i]) { 
+        
+        // 스택에 있는 것 보다 현재 타워가 크면 스택에서 pop
+        stack.pop();
+      } else {
+        break;
       }
-      x = x + dx[d];
-      y = y + dy[d];
     }
 
-    //밖으로 나갈시의 좌표기반으로 숫자를 리턴
-    if (x === -1) return 2 * M + 2 * N - y;
-    if (x === N) return N + y + 1;
-    if (y === -1) return x + 1;
-    if (y === M) return 2 * N + M - x;
-  };
+    if (!stack.length) result.push(0);
+    else {
 
-  let result = [];
-  for (let i = 0; i < N; i++) {
-    let d = 3;
-    result.push(move(d, i, 0));
-    //0 0
-    //1 0
-  }
-
-  for (let i = 0; i < M; i++) {
-    let d = 0;
-    result.push(move(d, N - 1, i));
-  }
-
-  for (let i = N - 1; i >= 0; i--) {
-    let d = 2;
-    result.push(move(d, i, M - 1));
-  }
-
-  for (let i = M - 1; i >= 0; i--) {
-    let d = 1;
-    result.push(move(d, 0, i));
+      // 스택에 값이 남아 있다면 가장 최근에 push된 것을 정답배열에 push
+      result.push(stack[stack.length - 1][1]);
+    }
   }
 
   console.log(result.join(' '));
 
+  //6 9 5 7 4
   process.exit();
 });
-
-// 0 1 0
-// 0 1 1
-
-//0 10 9 8 0
-//1  0 x 0 7
-//2  0 x x 6
-//0  3 4 5 0
-
-//왼쪽에서 쏘면 위로
-//오른쪽에서 쏘면 아래로
-
-//위에서 쏘면 왼쪽으로
-//아래에서 쏘면 오른쪽으로
