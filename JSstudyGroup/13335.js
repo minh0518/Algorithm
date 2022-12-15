f616fce4c9a77db463fb41d9b08c157e069879ac
minh0518 <rubics@naver.com>
2022-12-15 17:29:25 +0900

    22.12.15(13335리팩토링)

--- JSstudyGroup/13335.js
+++ JSstudyGroup/13335.js
@@ -1,41 +1,49 @@
-const readline = require('readline')
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-})
+});
 
-const data = []
+const data = [];
 
 rl.on('line', (input) => {
-  data.push(input)
+  data.push(input);
 }).on('close', () => {
-  let [N, W, L] = data.shift().split(' ').map(Number)
+  let [N, W, L] = data.shift().split(' ').map(Number);
 
-  let weights = data.shift().split(' ').map(Number)
+  let trucks = data.shift().split(' ').map(Number);
+  //다리들을 건너지 않은 대기중인 트럭들
 
-  let count = 0
-  let queue = new Array(W).fill(0)
+  let count = 0;
+  let bridge = new Array(W).fill(0);
 
-  while (queue.length) {
-    queue.shift()
-    count++
+  while (bridge.length) {
+    bridge.shift();
+    count += 1;
 
-    let currentTruck = weights[0]
-    if (weights.length) {
-      if (queue.reduce((a, b) => a + b, 0) + currentTruck > L) {//못 들어감
-        queue.push(0) //큐의 왼쪽에서 땡김
+    if (trucks.length) {
+
+      // 남은 트럭이 있는 경우에만 진행
+      // 남은 트럭이 없으면 bridge가 없어질 때까지 count+=1만 진행
+      // (=다리 위에 있는 마지막 트럭이 마저 건널때까지 count)
+
+      let currentTruck = trucks[0];
+      let currentWeightOnBridge = bridge.reduce((a, b) => a + b, 0);
+
+      if (currentWeightOnBridge + currentTruck > L) { // 다리 하중때문에 못 들어감
+        bridge.push(0); // 큐의 왼쪽에서 땡김
       } else {
-        queue.push(currentTruck) //큐의 오른쪽에 추가
-        weights.shift()
+        bridge.push(currentTruck); // 큐의 오른쪽에 추가
+        trucks.shift(); // 다리를 건너기 시작한 트럭은 trucks에서 제거
       }
     }
   }
 
-  console.log(count)
+  console.log(count);
 
-  process.exit()
-})
+  process.exit();
+});
 
 //다리에는 한번에 최대 w대의 트럭만 올라갈 수 있음
 //다리를 건너려면 다리에서 w번 이동해야 함
--- test.js
+++ test.js
@@ -1,49 +1,54 @@
-const readline = require('readline');
+const readline = require('readline')
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
 
-const data = [];
+const data = []
 
 rl.on('line', (input) => {
-  data.push(input);
+  data.push(input)
 }).on('close', () => {
+  let [N, W, L] = data.shift().split(' ').map(Number)
 
-  let [N, W, L] = data.shift().split(' ').map(Number);
-  //트럭수 , 다리길이, 최대하중
+  let trucks = data.shift().split(' ').map(Number)
+	//다리들을 건너지 않은 대기중인 트럭들
 
-  let trucks = data.shift().split(' ').map(Number);
+  let count = 0
+  let bridge = new Array(W).fill(0)
 
-  let queue = []; // 다리
+  while (bridge.length) {
+		//console.log(bridge)
+    bridge.shift()
+    //console.log(bridge)
+    count+=1
 
-  let count = 0;
+    if (trucks.length) { // 남은 트럭이 있는 경우에만 진행
+                        // 남은 트럭이 없으면 bridge가 없어질 때까지 count+=1만 진행
+                        // (=다리 위에 있는 마지막 트럭이 마저 건널때까지 count)
 
-  while (trucks.length) {
-    console.log(`trucks ${trucks} ,  queue ${queue}`);
-    let currentTruck = trucks[0]; //바로 shift를 하면 안된다
+	    let currentTruck = trucks[0]
+      let currentWeightOnBridge=bridge.reduce((a, b) => a + b, 0)
 
-    if (queue.length === W) { // 다리에 공간이 없을 때
-      queue.shift();
-    }
-    if (queue.length < W) { // 다리에 공간이 있을 때
-
-      let currentWeightOnBridge = queue.reduce((a, b) => a + b, 0);
-
-      if (currentTruck + currentWeightOnBridge <= L) { //다리에 추가 진입 가능하면
-        trucks.shift(); // 비로소 트럭배열에서 제거
-        queue.push(currentTruck);
-      } 
-      else { //다리에 추가 진입이 안되면
-        queue.push(0);
+      if (currentWeightOnBridge + currentTruck > L) {//다리 하중때문에 못 들어감
+        bridge.push(0) //큐의 왼쪽에서 땡김
+      } else {
+        bridge.push(currentTruck) //큐의 오른쪽에 추가
+        trucks.shift() //다리를 건너기 시작한 트럭은 trucks에서 제거
       }
     }
-
-    count += 1;
+		//console.log(bridge)
+    //console.log('====================')
   }
 
-  console.log(count + W);
+  console.log(count)
 
-  process.exit();
-});
+  process.exit()
+})
+
+//다리에는 한번에 최대 w대의 트럭만 올라갈 수 있음
+//다리를 건너려면 다리에서 w번 이동해야 함
+//최대하중 L
+
+//마지막까지 다 넘어간 시간까지 체크해야 함
