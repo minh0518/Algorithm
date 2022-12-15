31dce3e0cc8ee509bf1faee5fca941f6bcf8a52b
minh0518 <rubics@naver.com>
2022-12-15 21:14:04 +0900

    22.12.15(1461리팩토링)

--- JSstudyGroup/1461.js
+++ JSstudyGroup/1461.js
@@ -1,91 +1,75 @@
-const readline = require('readline')
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-})
+});
 
-const data = []
+const data = [];
 
 rl.on('line', (input) => {
-  data.push(input)
+  data.push(input);
 }).on('close', () => {
-  let [N, M] = data.shift().split(' ').map(Number)
-  let cord = data
-    .shift()
-    .split(' ')
-    .map(Number)
-    .sort((a, b) => a - b)
+  let [N, M] = data.shift().split(' ').map(Number);
+  //한번에 M개
 
-  // [ -39, -37, -29, -28, -6,   2,  11]
+  let books = data.shift().split(' ').map(Number);
 
-  // 가장 먼 곳 : 39
-  // > 39 37 은 맨 마지막에 (39만 가는게 아니라 37까지도 가야 하는 것이 핵심이다)
+  let left = [];
+  let right = [];
+  for (let i of books) {
+    if (i < 0) left.push(-i);
+    if (i > 0) right.push(i);
+  }
 
-  // 11 *2 (양)
-  //29*2 (29 28까지) >> 6 28 | 29 냐  ,  6 | 28 29 냐 이것 중에선 후자다
-  //                    이건 28*2+29*2고         6*2+29*2이기 때문
-  //                    그니까 한번 갔다가 돌아오는 경우는 최대한 멀리갔다가 와야 한다
-  //                    안 그러면 또 다시 가야함 (28까지 갔다 29 가는 것보다 29가면서 28까지 해결하는게 나음)
-  //6*12
-  //39
+	// 최댓값 찾음
+  let max = Math.max(...left, ...right);
 
-  let plus = [] // 양수에 위치하는 책들
-  let minus = [] // 음수에 위치하는 책들
+  // 내림차순 정렬 ([0]이 가장 큰 값)
+  left = left.sort((a, b) => b - a);
+  right = right.sort((a, b) => b - a);
 
-  cord.map((i) => {
-    if (i > 0) {
-      plus.push(i)
+  let result = [];
+
+	// 가장 큰 값이면 한번만 더해주는 것고
+	// 그게 아니라면 *2해서 더해준다
+	// 그리고 매번 M만큼 잘라주는데 어차피 
+	// 배열의 길이가 splice의 M보다 작아도 알아서 남은만큼 잘라준다
+  while (left.length) {
+    let firstValue = left[0];
+    if (firstValue === max) {
+      result.push(firstValue);
     } else {
-      minus.push(Math.abs(i))
+      result.push(firstValue * 2);
     }
-  })
-
-  plus = plus.sort((a, b) => b - a)
-  minus = minus.sort((a, b) => b - a)
-
-  let result = []
-  if (!minus.length || plus[0] > minus[0]) { //minus가 undefined면 비교가 안되니까 조건 추가
-    result.push(plus[0])
-    plus = plus.slice(M) //plus가 없어도
-  }
-  // else if(plus[0]===minus[0]){
-  // }
-  else if (!plus.length || plus[0] <= minus[0]) {
-    result.push(minus[0])
-    minus = minus.slice(M)
+    left.splice(0, M);
   }
 
-  while (minus.length || plus.length) {
-    if (minus.length) {
-      if (minus.length >= 2) {
-        result.push(minus[0] * 2)
-        minus = minus.slice(M)
-      } else {
-        result.push(minus[0] * 2)
-        minus = minus.slice(1)
-      }
+  while (right.length) {
+    let firstValue = right[0];
+    if (firstValue === max) {
+      result.push(firstValue);
+    } else {
+      result.push(firstValue * 2);
     }
-
-    if (plus.length) {
-      if (plus.length >= 2) {
-        result.push(plus[0] * 2)
-        plus = plus.slice(M)
-      } else {
-        result.push(plus[0] * 2)
-        plus = plus.slice(1)
-      }
-    }
+    right.splice(0, M);
   }
 
-  console.log(result.reduce((a, b) => a + b, 0))
+  console.log(result.reduce((a,b)=>a+b,0))
+  process.exit();
+});
 
-  process.exit()
-})
+// -39, -37, -29, -28, -6,   0   2,  11
+//28 28
+//37 37
+//39
 
-//가장 큰 값이 같을때도 상관이 없는게
-// [ 39, 12, 11, 2 ] [ 39, 37, 36, 35, 6 ]
-//어차피 둘 중 하나를 지우면 다른 39까지 가야 한다
-//그러면 M이 2니까 가는 김에 39 다음의 값도 알아서 지워지게 된다
-//그러므로 39로 둘 다 같더라도 굳이 다음의 수까지 고려하면서
-//끝까지 둘 중 어느 것에서 빼야할지 정하지 않아도 된다
+//6 6
+//29 29
+//39
+
+//(가장 절댓값이 큰 것을 마지막 경로로)
+// 2  11  15
+// 2 2
+// 15 15
+//(가장 마지막에 있는 값은 가는 길에 같이 가지고 가야 효율적)
--- test.js
+++ test.js
@@ -1,54 +1,95 @@
-const readline = require('readline')
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-})
+});
 
-const data = []
+const data = [];
 
 rl.on('line', (input) => {
-  data.push(input)
+  data.push(input);
 }).on('close', () => {
-  let [N, W, L] = data.shift().split(' ').map(Number)
+  let [N, M] = data.shift().split(' ').map(Number);
+  let cord = data
+    .shift()
+    .split(' ')
+    .map(Number)
+    .sort((a, b) => a - b);
 
-  let trucks = data.shift().split(' ').map(Number)
-	//다리들을 건너지 않은 대기중인 트럭들
+  // [ -39, -37, -29, -28, -6,   2,  11]
 
-  let count = 0
-  let bridge = new Array(W).fill(0)
+  // 가장 먼 곳 : 39
+  // > 39 37 은 맨 마지막에 (39만 가는게 아니라 37까지도 가야 하는 것이 핵심이다)
 
-  while (bridge.length) {
-		//console.log(bridge)
-    bridge.shift()
-    //console.log(bridge)
-    count+=1
+  // 11 *2 (양)
+  //29*2 (29 28까지) >> 6 28 | 29 냐  ,  6 | 28 29 냐 이것 중에선 후자다
+  //                    이건 28*2+29*2고         6*2+29*2이기 때문
+  //                    그니까 한번 갔다가 돌아오는 경우는 최대한 멀리갔다가 와야 한다
+  //                    안 그러면 또 다시 가야함 (28까지 갔다 29 가는 것보다 29가면서 28까지 해결하는게 나음)
+  //6*12
+  //39
 
-    if (trucks.length) { // 남은 트럭이 있는 경우에만 진행
-                        // 남은 트럭이 없으면 bridge가 없어질 때까지 count+=1만 진행
-                        // (=다리 위에 있는 마지막 트럭이 마저 건널때까지 count)
+  let plus = []; // 양수에 위치하는 책들
+  let minus = []; // 음수에 위치하는 책들
 
-	    let currentTruck = trucks[0]
-      let currentWeightOnBridge=bridge.reduce((a, b) => a + b, 0)
-
-      if (currentWeightOnBridge + currentTruck > L) {//다리 하중때문에 못 들어감
-        bridge.push(0) //큐의 왼쪽에서 땡김
-      } else {
-        bridge.push(currentTruck) //큐의 오른쪽에 추가
-        trucks.shift() //다리를 건너기 시작한 트럭은 trucks에서 제거
-      }
+  cord.map((i) => {
+    if (i > 0) {
+      plus.push(i);
+    } else {
+      minus.push(Math.abs(i));
     }
-		//console.log(bridge)
-    //console.log('====================')
+  });
+
+  plus = plus.sort((a, b) => b - a);
+  minus = minus.sort((a, b) => b - a);
+
+  let result = [];
+  if (!minus.length || plus[0] > minus[0]) {
+    //minus가 undefined면 비교가 안되니까 조건 추가
+    result.push(plus[0]);
+    plus = plus.slice(M); //plus가 없어도
+  }
+  // else if(plus[0]===minus[0]){
+  // }
+  else if (!plus.length || plus[0] <= minus[0]) {
+    result.push(minus[0]);
+    minus = minus.slice(M);
   }
 
-  console.log(count)
+  while (minus.length || plus.length) {
+    if (minus.length) {
+      if (minus.length >= 2) {
+        result.push(minus[0] * 2);
+        minus = minus.slice(M);
+      } else {
+        result.push(minus[0] * 2);
+        minus = minus.slice(1);
+      }
+    }
 
-  process.exit()
-})
+    if (plus.length) {
+      if (plus.length >= 2) {
+        result.push(plus[0] * 2);
+        plus = plus.slice(M);
+      } else {
+        result.push(plus[0] * 2);
+        plus = plus.slice(1);
+      }
+    }
+  }
 
-//다리에는 한번에 최대 w대의 트럭만 올라갈 수 있음
-//다리를 건너려면 다리에서 w번 이동해야 함
-//최대하중 L
+  console.log(result.reduce((a, b) => a + b, 0));
 
-//마지막까지 다 넘어간 시간까지 체크해야 함
+  process.exit();
+});
+
+
+//가장 큰 값이 같을때도 상관이 없는게
+// [ 39, 12, 11, 2 ] [ 39, 37, 36, 35, 6 ]
+//어차피 둘 중 하나를 지우면 다른 39까지 가야 한다
+//그러면 M이 2니까 가는 김에 39 다음의 값도 알아서 지워지게 된다
+//그러므로 39로 둘 다 같더라도 굳이 다음의 수까지 고려하면서
+//끝까지 둘 중 어느 것에서 빼야할지 정하지 않아도 된다
+
+
