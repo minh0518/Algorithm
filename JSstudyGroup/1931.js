644a139e6fd1ab905cc8357bcece011e958127f8
minh0518 <rubics@naver.com>
2022-12-26 16:23:10 +0900

    22.12.26

--- JSstudyGroup/1931.js
+++ JSstudyGroup/1931.js
@@ -15,21 +15,20 @@ rl.on('line', (input) => {
   let time = data.map((i) => i.split(' ').map(Number));
 
   time = time.sort((a, b) => {
-    return (a[1] - b[1]||a[0]-b[0]);
+    return a[1] - b[1] || a[0] - b[0];
   });
 
-  let target=time[0][1]
+  let target = time[0][1];
 
-  let result=1
-  for(let i=1; i<time.length; i++){
-    if(time[i][0]>=target) {
-      result+=1
-      target=time[i][1]
+  let result = 1;
+  for (let i = 1; i < time.length; i++) {
+    if (time[i][0] >= target) {
+      result += 1;
+      target = time[i][1];
     }
   }
 
-
-  console.log(result)  
+  console.log(result);
 
   process.exit();
 });
