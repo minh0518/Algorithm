4ab9526d769b7daf95cbb3fad672113dbf186c90
minh0518 <rubics@naver.com>
2023-03-15 01:15:52 +0900

    23.3.15

--- JSstudyGroup/1644.js
+++ JSstudyGroup/1644.js
@@ -34,24 +34,28 @@ rl.on('line', (input) => {
   let start = 0;
   let end = 1;
   let count = 0;
+
+  let sum = primeNumbers[start] + primeNumbers[end];
   while (end < primeNumbers.length - 1) {
-    let tmp = primeNumbers.slice(start, end);
-    let sum = tmp.reduce((a, b) => a + b, 0);
     if (sum === N) {
       count += 1;
       end += 1;
+      sum += primeNumbers[end];
       continue;
     }
+
     if (sum > N) {
+      sum -= primeNumbers[start];
       start += 1;
-      continue;
     }
+
     if (sum < N) {
       end += 1;
-      continue;
+      sum += primeNumbers[end];
     }
   }
 
+  // 마지막 소수가 자기 자신인 경우
   if (primeNumbers[primeNumbers.length - 1] === N) count += 1;
 
   console.log(count);
