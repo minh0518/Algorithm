60f40d70f050f02b6c64391abd4b9c66d548e769
minh0518 <rubics@naver.com>
2023-03-29 22:25:16 +0900

    23.3.29

--- JSstudyGroup/14502.js
+++ JSstudyGroup/14502.js
@@ -14,14 +14,13 @@ rl.on('line', (input) => {
 
   let lab = data.map((i) => i.split(' ').map(Number));
 
-  let answer = 0;
-
+  // 0의 좌표 , 2의 좌표를 미리 받아 둠
   let zero = [];
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-      if (lab[i][j] === 0) {
-        zero.push([i, j]);
-      }
+  let two = [];
+  for (let i = 0; i < lab.length; i++) {
+    for (let j = 0; j < lab[i].length; j++) {
+      if (lab[i][j] === 0) zero.push([i, j]);
+      if (lab[i][j] === 2) two.push([i, j]);
     }
   }
 
@@ -30,16 +29,9 @@ rl.on('line', (input) => {
     [0, 0, -1, 1],
   ];
 
-  // 각 경우에 대해서 bfs로 0의 범위를 구함
-  const countZero = (lab) => {
-    let cnt = 0;
-    let queue = [];
-
-    for (let i = 0; i < N; i++) {
-      for (let j = 0; j < M; j++) {
-        if (lab[i][j] === 2) queue.push([i, j]);
-      }
-    }
+  // 0의 범위를 구하는 bfs
+  const bfs = (lab) => {
+    let queue = JSON.parse(JSON.stringify(two));
 
     while (queue.length) {
       let [x, y] = queue.shift();
@@ -47,7 +39,6 @@ rl.on('line', (input) => {
       for (let i = 0; i < 4; i++) {
         let nx = x + dx[i];
         let ny = y + dy[i];
-
         if (nx >= 0 && nx < N && ny >= 0 && ny < M && lab[nx][ny] === 0) {
           lab[nx][ny] = 2;
           queue.push([nx, ny]);
@@ -55,25 +46,28 @@ rl.on('line', (input) => {
       }
     }
 
-    for (let i = 0; i < N; i++) {
-      for (let j = 0; j < M; j++) {
-        if (lab[i][j] === 0) {
-          cnt += 1;
+    const countZero = () => {
+      let count = 0;
+      for (let i = 0; i < lab.length; i++) {
+        for (let j = 0; j < lab[i].length; j++) {
+          if (lab[i][j] === 0) count += 1;
         }
       }
-    }
+      return count;
+    };
 
-    return cnt;
+    return countZero();
   };
 
-  // let result=[]
 
-  // 백트래킹으로 벽을 세울 수 있는 모든 경우의 수를 구함
+  // 1을 세울 수 있는 모든 경우의 수를 구하기 위한 백트래킹
+  let result = [];
   const dfs = (index, depth) => {
     if (depth === 3) {
-      let safe = countZero(JSON.parse(JSON.stringify(lab)));
-      // result.push(safe)
-      answer = safe > answer ? safe : answer;
+
+			// 반드시 깊은 복사로 넘겨줘야 한다
+      let zeroZone = bfs(JSON.parse(JSON.stringify(lab)));
+      result.push(zeroZone);
       return;
     }
 
@@ -81,14 +75,14 @@ rl.on('line', (input) => {
       let [x, y] = zero[i];
       lab[x][y] = 1;
       dfs(i + 1, depth + 1);
+
       lab[x][y] = 0;
     }
   };
-
+  
   dfs(0, 0);
 
-  // console.log(Math.max(...result));
-  console.log(answer);
+  console.log(Math.max(...result));
 
   process.exit();
 });
