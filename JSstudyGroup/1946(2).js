4b1e928d44b09b9c2c9133f006d80ccf32a162d9
minh0518 <rubics@naver.com>
2023-10-21 16:19:53 +0900

    23.10.21

--- JSstudyGroup/1946(2).js
+++ JSstudyGroup/1946(2).js
@@ -16,33 +16,26 @@ rl.on('line', (input) => {
   while (T--) {
     const N = +data[index];
     const info = data.slice(index + 1, index + 1 + N).map((i) => i.split(' ').map(Number));
+    info.sort((a, b) => a[0] - b[0]);
 
-    info.sort((a, b) => {
-      return a[0] - b[0];
-    });
+    // 최소 인터뷰 컷 점수
+    let cutLine = info[0][1];
 
-    let count = 0;
-    // 최소 인터뷰 컷
-    let minimumInterviewCut = info[0][1];
+    let minusCount = 0;
+
     for (let i = 1; i < info.length; i++) {
-      const currentInterview = info[i][1];
-
-      // 최소 인터뷰 컷보다 낮은 등수들은 제거
-      if (minimumInterviewCut < currentInterview) {
-        count += 1;
+      // 최소 커트라인 갱신
+      if (cutLine > info[i][1]) {
+        cutLine = info[i][1];
+        continue;
       }
-      // 최소 인터뷰 컷보다 높다면 이걸 최소 인터뷰 컷으로
-      // (이 문제는 '단 한명이라도' 점수가 높은 사람이 있으면 탈락이므로)
-      if (minimumInterviewCut > currentInterview) {
-        minimumInterviewCut = currentInterview;
-      }
+      // 최소 인터뷰 커트라인보다 등수가 낮다면 탈락
+      minusCount += 1;
     }
-
-    result.push(N - count);
+    result.push(N - minusCount);
 
     index += N + 1;
   }
-
   console.log(result.join('\n'));
 
   process.exit();
