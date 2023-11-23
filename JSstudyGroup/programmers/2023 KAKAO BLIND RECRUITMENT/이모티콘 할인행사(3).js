cf86240a93379b5104f33493ad25758fe8dd586f
minh0518 <rubics@naver.com>
2023-11-23 21:33:13 +0900

    23.11.23

---
+++ JSstudyGroup/programmers/2023 KAKAO BLIND RECRUITMENT/이모티콘 할인행사(3).js
@@ -0,0 +1,66 @@
+/*
+ * @url https://school.programmers.co.kr/learn/courses/30/lessons/150368
+ * @date 23-11-23
+ */
+
+function solution(users, emoticons) {
+  const DISCOUNT_RATES = [10, 20, 30, 40];
+
+  // 각 할인율 경우에 따른 계산
+  const calcResult = (discountArr) => {
+    const discountedPriceArr = [...emoticons].map((i, index) => {
+      const currentDiscount = discountArr[index];
+      return i - i * (currentDiscount / 100);
+    });
+
+    let totalPrice = 0;
+    let totalEnroll = 0;
+    for (let i = 0; i < users.length; i++) {
+      const [percent, price] = users[i];
+
+      let buyListPrice = 0;
+      let isEnroll = false;
+
+      for (let j = 0; j < discountArr.length; j++) {
+        if (percent <= discountArr[j]) {
+          buyListPrice += discountedPriceArr[j];
+        }
+        if (buyListPrice >= price) {
+          isEnroll = true;
+        }
+      }
+
+      if (isEnroll) {
+        totalEnroll += 1;
+      }
+      if (!isEnroll) {
+        totalPrice += buyListPrice;
+      }
+    }
+
+    return [totalEnroll, totalPrice];
+  };
+
+  let result = [];
+
+  // 백트래킹
+  const dfs = (current) => {
+    if (current.length === emoticons.length) {
+      result.push(calcResult(current));
+      return;
+    }
+
+    for (let i = 0; i < DISCOUNT_RATES.length; i++) {
+      current.push(DISCOUNT_RATES[i]);
+      dfs(current);
+      current.pop();
+    }
+  };
+
+  dfs([]);
+
+  return result.sort((a, b) => {
+    if (a[0] === b[0]) return b[1] - a[1];
+    return b[0] - a[0];
+  })[0];
+}
