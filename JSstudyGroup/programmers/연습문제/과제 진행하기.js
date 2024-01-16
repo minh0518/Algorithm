ed139e7d5921f57e84bd01a0f09f19f6aa83b24a
minh0518 <rubics@naver.com>
2024-01-16 12:14:32 +0900

    24.1.16

--- JSstudyGroup/programmers/연습문제/과제 진행하기.js
+++ JSstudyGroup/programmers/연습문제/과제 진행하기.js
@@ -1,86 +1,53 @@
 function solution(plans) {
-  let answer = [];
+  // 23:59를 분 단위의 배열로 표현
+  const timeTalbe = new Array(23 * 59 + 59).fill(0);
 
-  plans = plans.map(([name, start, playtime]) => {
-    const [hh, mm] = start.split(':').map(Number);
+  plans = plans.map((i) => {
+    const [h, m] = i[1].split(':').map(Number);
+    const convertedTime = h * 60 + m;
 
-    return { name, start: hh * 60 + mm, playtime: Number(playtime) };
+    // 각 과제의 시작 지점을 timeTalbe배열에도 추가
+    timeTalbe[convertedTime] = { task: i[0], value: Number(i[2]) };
+
+    return [i[0], convertedTime, Number(i[2])];
   });
+  plans.sort((a, b) => a[1] - b[1]);
 
-  console.log(plans);
-  // [
-  //   { name: 'science', start: 760, playtime: 50 },
-  //   { name: 'music', start: 740, playtime: 40 },
-  //   { name: 'history', start: 840, playtime: 30 },
-  //   { name: 'computer', start: 750, playtime: 100 },
-  // ];
-
-  // 24시간을 분 단위의 배열로 표현
-  // 크기가 1440인 배열
-  const timeLine = Array(24 * 60).fill(null);
-
-  // 각 과제의 시작 지점을 체크
-  plans.forEach((v) => {
-    timeLine[v.start] = v;
-  });
-
-  // 과제들이 담기는 곳 (현재 진행 , 남은 과제 모두 포함)
   const stack = [];
+  const result = [];
 
-  //00:00~23:59를 이중 for문으로 반복
-  for (let h = 0; h < 24; h++) {
-    for (let m = 0; m < 60; m++) {
-      // 이 로직에서 사용되고 있는 분 단위로 환산
-      const time = h * 60 + m;
+  //00:00~23:59를 for문으로 반복
+  for (let i = 0; i < timeTalbe.length; i++) {
+    const currentTime = timeTalbe[i];
 
-      const plan = timeLine[time];
-      // 현재 시간에 진행해야 하는 과제가 있다면 스택에 push
-      // 즉 새로운 과제가 진행되는 것이다
-      if (plan) {
-        stack.push(plan);
-      }
+    // 현재 시간에 진행해야 하는 과제가 있다면 스택에 push
+    // 즉 새로운 과제가 진행되는 것이다
+    if (currentTime) {
+      stack.push({ task: currentTime.task, value: currentTime.value });
+    }
 
-      // 스택에 무언가라도 들어있다면(=현재 시간에 진행 해야 할 과제 or 이전에 남은 과제가 있다면)
-      if (stack.length) {
-        //가장 위에 있는 과제
-        const top = stack[stack.length - 1];
+    // 스택에 무언가라도 들어있다면(=현재 시간에 진행 해야 할 과제 or 이전에 남은 과제가 있다면)
+    if (stack.length) {
+      //가장 위에 있는 과제
+      const currentTask = stack[stack.length - 1];
 
-        // 1분씩 줄여나가는데 1분 줄이면 과제가 끝난다면
-        if (!(top.playtime - 1)) {
-          answer.push(top.name); // 정답에 추가
-          stack.pop(); // 제거
-        } else {
-          top.playtime--; // for문은 1분씩 진행되므로 1분씩 차감
-        }
+      // 1분당 과제를 진행
+      currentTask.value -= 1; // call by reference로 수정
+      if (currentTask.value === 0) {
+        result.push(stack.pop().task);
       }
     }
   }
 
+  // 위의 for문에서는 23:59까지 시작한 과제들을 전부 스택에 두고
+  // 23:59까지 끝난 과제까지 처리하지만
   // 23:59 넘어서 끝나는 과제들이 있을 수 있으므로
-  // 이건 늦게 시작한 순서대로 정답배열에 추가
+  // 최근에 시작했던 순서대로 정답배열에 추가
   // 23:59 부터는 더이상 추가로 진행해야 할 과제가 없는 것이므로
   // 최근에 시작한 것부터 끝내게 되는 것이다
   while (stack.length) {
-    const top = stack.pop();
-
-    answer.push(top.name);
+    result.push(stack.pop().task);
   }
 
-  return answer;
+  return result;
 }
-// solution([
-//   ['korean', '11:40', '30'],
-//   ['english', '12:10', '20'],
-//   ['math', '12:30', '40'],
-// ]);
-solution([
-  ['science', '12:40', '50'],
-  ['music', '12:20', '40'],
-  ['history', '14:00', '30'],
-  ['computer', '12:30', '100'],
-]);
-// solution([
-//   ['aaa', '12:00', '20'],
-//   ['bbb', '12:10', '30'],
-//   ['ccc', '12:40', '10'],
-// ]);
