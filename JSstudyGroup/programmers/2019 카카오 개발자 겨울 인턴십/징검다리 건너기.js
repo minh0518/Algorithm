4cae7368bb15990feef722442e90805208c72019
minh0518 <rubics@naver.com>
2024-04-26 17:49:12 +0900

    24.4.26

--- JSstudyGroup/programmers/2019 카카오 개발자 겨울 인턴십/징검다리 건너기.js
+++ JSstudyGroup/programmers/2019 카카오 개발자 겨울 인턴십/징검다리 건너기.js
@@ -1,26 +1,36 @@
-const solution = (stones, k) => {
+function solution(stones, k) {
   let left = 1;
   let right = 200000000;
 
-  while (left <= right) {
-    let mid = parseInt((left + right) / 2);
-    let count = 0;
-    let copyStones = [...stones];
-    for (let i = 0; i < copyStones.length; i++) {
-      // 뺄셈을 한 결과를 다시 사용하는게 아니라
-      // 그 자리에서 삼항연산자로 바로 비교
-      copyStones[i] - mid <= 0 ? (count += 1) : (count = 0);
+  // 현재 mid값으로 건널 수 있는지 확인
+  const calc = (mid) => {
+    let zeroCount = 0; // 연속된 0의 갯수
 
-      if (count === k) {
-        right = mid - 1;
-        break;
-      }
+    for (let i = 0; i < stones.length; i++) {
+      let subResult = stones[i] - mid;
+      if (subResult <= 0) zeroCount += 1;
+      if (subResult > 0) zeroCount = 0; // count가 연속된 갯수를 세기 위해 0이 아니면 0으로 다시 초기화
+
+      if (zeroCount === k) return false;
+    }
+    return true;
+  };
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    const isPossible = calc(mid);
+
+    // 0보다 작거나 같은 값이 k만큼 연속된다면 건널 수 없음
+    if (!isPossible) {
+      right = mid - 1;
     }
 
-    if (count !== k) {
+    // 0보다 작거나 같은 값이 k보다 작다면 건널 수 있음
+    if (isPossible) {
       left = mid + 1;
     }
   }
 
   return left;
-};
+}
