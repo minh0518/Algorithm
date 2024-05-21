322f51d3dc46dab479dfbb4acea365e533b80a40
minh0518 <rubics@naver.com>
2024-05-21 22:23:44 +0900

    24.5.21

--- JSstudyGroup/1939.js
+++ JSstudyGroup/1939.js
@@ -14,43 +14,48 @@ rl.on('line', (input) => {
   const info = data.slice(0, M).map((i) => i.split(' ').map(Number));
   const [start, end] = data.pop().split(' ').map(Number);
   const board = new Array(N + 1).fill(undefined).map(() => []);
+  let maxValue = 0;
 
+  // 양방향 그래프 생성
   for (let [from, to, value] of info) {
+    if (maxValue < value) maxValue = value;
     board[from].push({ to, value });
     board[to].push({ to: from, value });
   }
 
-  const calc = (from, to, target) => {
-    const bfs = () => {
-      const queue = [];
-      queue.push(from);
-      const visited = new Array(N + 1).fill(false);
-      visited[from] = true;
+  // 이분탐색에서 주어진 target 중량으로,
+  // from에서 to까지 운반이 가능한지 확인
 
-      while (queue.length) {
-        const currentNode = queue.shift();
+  const bfs = (target) => {
+    const queue = [];
+    queue.push(start);
+    const visited = new Array(N + 1).fill(false);
+    visited[start] = true;
 
-        const nextNodes = board[currentNode];
+    while (queue.length) {
+      const currentNode = queue.shift();
 
-        for (let nodeInfo of nextNodes) {
-          const nextNode = nodeInfo.to;
-          if (visited[nextNode] || target > nodeInfo.value) continue;
-          if (nextNode === to) return true;
-          visited[nextNode] = true;
-          queue.push(nextNode);
-        }
+      const nextNodes = board[currentNode];
+
+      for (let nodeInfo of nextNodes) {
+        const nextNode = nodeInfo.to;
+        // 현재 target중량으로 다음 섬으로 가는 다리를 건널 수 없으면 continue
+        if (visited[nextNode] || target > nodeInfo.value) continue;
+        if (nextNode === end) return true; // 목적지로 방문 가능하다면 true반환
+        visited[nextNode] = true;
+        queue.push(nextNode);
       }
-      return false;
-    };
-    return bfs();
+    }
+    return false;
   };
 
+  // 이분탐색
   let left = 0;
-  let right = 1000000000;
+  let right = maxValue;
   while (left <= right) {
     const mid = Math.floor((left + right) / 2);
 
-    const isPossible = calc(start, end, mid);
+    const isPossible = bfs(mid);
 
     if (isPossible) {
       left = mid + 1;
