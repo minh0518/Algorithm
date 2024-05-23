872a36f3b771f615c8f6c17bb1f9c368165fd644
minh0518 <rubics@naver.com>
2024-05-23 14:20:53 +0900

    24.5.23

--- JSstudyGroup/programmers/연습문제/배달(bfs).js
+++ JSstudyGroup/programmers/연습문제/배달(bfs).js
@@ -1,35 +1,42 @@
 function solution(N, road, K) {
-  // 각 노드의 최단 비용을 담는 배열
-  let result = new Array(N + 1).fill(500000);
-  // 시작 지점은 1번 노드는 비용이 0
-  result[1] = 0;
+  const board = new Array(N + 1).fill(undefined).map(() => []);
 
-  // 그래프 생성
-  const graph = new Array(N + 1).fill().map(() => []);
-
-  for (let i of road) {
-    let [from, to, value] = i;
-    graph[from].push({ to, value });
-    graph[to].push({ to: from, value });
+  for (let [from, to, value] of road) {
+    board[from].push([to, value]);
+    board[to].push([from, value]);
   }
 
-  let queue = [{ to: 1, value: 0 }];
+  const bfs = () => {
+    // 방문노드를 담는 정답 배열
+    const result = new Set();
+    result.add(1);
 
-  while (queue.length) {
-    let currentNode = queue.shift();
+    const queue = [];
+    queue.push([1, 0]); // [노드,현재 노드까지의 비용]
 
-    // 현재 노드로부터 방문할 수 있는 노드들을 순회
-    for (let i of graph[currentNode.to]) {
-      let { to, value } = i;
+    // 노드별 비용 기록 배열
+    const dp = new Array(N + 1).fill(Infinity);
+    dp[1] = 0; // 시작지점은 비용이 0
 
-      // 방문 가능한 노드(to)로 가는데 사용되는 비용이 정답배열에 비용보다 작다면 갱신
-      // 정답배열에 존재하는 to까지 걸리는 비용 >  현재 노드까지 걸린 비용 + to로 가는데 사용되는 비용
-      if (result[to] > currentNode.value + value) {
-        result[to] = currentNode.value + value; //
-        queue.push({ to: to, value: value + currentNode.value }); // queue에도 추가 해주는데 value값을 누적한 값으로 추가해야 한다
+    while (queue.length) {
+      const [currentNode, currentValue] = queue.shift();
+      const nextNodes = board[currentNode];
+
+      for (let i = 0; i < nextNodes.length; i++) {
+        const [nodeNumber, value] = nextNodes[i];
+        const nextValue = currentValue + value;
+
+        // 다음 노드방문시 K를 초과하거나,
+        // 기존에 다른 경로로 방문했던 비용 이상이라면 패스
+        if (nextValue > K || dp[nodeNumber] <= nextValue) continue;
+
+        queue.push([nodeNumber, nextValue]);
+        result.add(nodeNumber); // 정답 기록
+        dp[nodeNumber] = nextValue; // dp배열 갱신
       }
     }
-  }
+    return result;
+  };
 
-  return result.filter((i) => i <= K).length;
+  return bfs().size;
 }
