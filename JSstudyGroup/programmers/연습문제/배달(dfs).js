120caa7216a3c180a8dda98659c3159125d8cac6
minh0518 <rubics@naver.com>
2024-05-23 14:19:51 +0900

    24.5.23

--- JSstudyGroup/programmers/연습문제/배달(dfs).js
+++ JSstudyGroup/programmers/연습문제/배달(dfs).js
@@ -1,33 +1,37 @@
 function solution(N, road, K) {
-  const graph = new Map();
-
   // 그래프 생성
-  for (let i of road) {
-    let [from, to, value] = i;
+  const board = new Array(N + 1).fill(undefined).map(() => []);
 
-    // 양방향
-    graph.has(from) ? graph.set(from, [...graph.get(from), [to, value]]) : graph.set(from, [[to, value]]);
-    graph.has(to) ? graph.set(to, [...graph.get(to), [from, value]]) : graph.set(to, [[from, value]]);
+  for (let [from, to, value] of road) {
+    board[from].push([to, value]); // [연결노드,비용]
+    board[to].push([from, value]);
   }
 
-  const visited = new Array(N + 1).fill(false);
-  visited[1] = true;
+  const result = new Set();
 
-  let result = [];
-  const dfs = (index, current, visited) => {
-    let adj = graph.get(index);
-    for (let i of adj) {
-      if (current + i[1] <= K && !visited[i[0]]) {
-        result.push(i[0]);
-        visited[i[0]] = true;
-        dfs(i[0], current + i[1], visited);
-        visited[i[0]] = false; // 해당 노드 방문 기록 초기화
-      }
+  // 현재 까지 누적 비용, 현재 노드, 방문 배열
+  const dfs = (currentValue, currentNode, visited) => {
+    // 정답 배열에 배달 가능 노드 기록
+    if (currentValue <= K) {
+      result.add(currentNode);
+    }
+
+    const nextNodes = board[currentNode];
+
+    for (let i = 0; i < nextNodes.length; i++) {
+      const [nodeNumber, value] = nextNodes[i];
+      const nextValue = currentValue + value;
+
+      // 현재 탐색 경로에서 이미 방문했거나, 비용이 K를 초과하면 패스
+      if (visited[nodeNumber] || nextValue > K) continue;
+
+      visited[nodeNumber] = true;
+      dfs(nextValue, nodeNumber, visited);
+      visited[nodeNumber] = false;
     }
   };
 
-  dfs(1, 0, visited);
+  dfs(0, 1, new Array(N + 1).fill(false));
 
-  // console.log(result)
-  return new Set(result).size + 1;
+  return result.size;
 }
