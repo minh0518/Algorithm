efbb95247d66e65d271c0ee252066bac91491814
minh0518 <rubics@naver.com>
2024-06-14 15:09:38 +0900

    24.6.14

--- JSstudyGroup/4485.js
+++ JSstudyGroup/4485.js
@@ -10,122 +10,55 @@ const data = [];
 rl.on('line', (input) => {
   data.push(input);
 }).on('close', () => {
-  class Heap {
-    constructor(compare = (a, b) => a < b) {
-      // default : 최소 힙
-      this.heap = [null];
-      this.compare = compare;
-    }
-    insert(item) {
-      this.heap.push(item);
-      let i = this.heap.length - 1;
-      while (i !== 1 && this.compare(item, this.heap[Math.floor(i / 2)])) {
-        this.heap[i] = this.heap[Math.floor(i / 2)];
-        i = Math.floor(i / 2);
-      }
-      this.heap[i] = item;
-    }
-    delete() {
-      const item = this.heap[1];
-      const tmp = this.heap.pop();
-      const len = this.heap.length - 1;
-      let parent = 1;
-      let child = 2;
-      while (child <= len) {
-        if (child < len && this.compare(this.heap[child + 1], this.heap[child]))
-          child += 1;
-        if (this.compare(tmp, this.heap[child])) break;
-        this.heap[parent] = this.heap[child];
-        parent = child;
-        child *= 2;
-      }
-      if (this.heap.length > 1) this.heap[parent] = tmp;
-      return item;
-    }
-    empty() {
-      return this.heap.length === 1;
-    }
-  }
+  let index = 0;
 
-  class PriorityQueue {
-    constructor(compare) {
-      this.heap = new Heap(compare);
-    }
-    empty() {
-      return this.heap.empty();
-    }
-    push(item) {
-      this.heap.insert(item);
-    }
-    pop() {
-      if (!this.empty()) return this.heap.delete();
-      else return null;
-    }
-  }
-
-  // 메인 로직
-  let [dx, dy] = [
+  const [dx, dy] = [
     [-1, 1, 0, 0],
     [0, 0, -1, 1],
   ];
-  const solution = () => {
-    let N = +data.shift();
-    let map = data.splice(0, N).map((i) => i.split(' ').map(Number));
 
-    const pq = new PriorityQueue((a, b) => a.cost < b.cost);
+  const result = [];
 
-    let distance = new Array(N).fill().map(() => new Array(N).fill(Infinity));
+  const bfs = (board, N) => {
+    // 최소비용을 담는 배열
+    const dp = new Array(N).fill(undefined).map(() => new Array(N).fill(Infinity));
+    // 시작 비용 초기화
+    dp[0][0] = board[0][0];
 
-    const bfs = (x, y) => {
-      pq.push({ to: [x, y], cost: map[x][y] });
+    const queue = [];
+    queue.push([0, 0]);
+    while (queue.length) {
+      // 행,열
+      const [x, y] = queue.shift();
+      for (let i = 0; i < 4; i++) {
+        const nx = x + dx[i];
+        const ny = y + dy[i];
+        if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
 
-      distance[x][y] = map[x][y];
+        const nextValue = dp[x][y] + board[nx][ny];
 
-      while (!pq.empty()) {
-        const { to, cost } = pq.pop();
-
-        // 하나의 좌료를 기점으로 상하좌우로 연결된 노드들에 대해 갱신 작업
-        for (let i = 0; i < 4; i++) {
-          let nx = to[0] + dx[i];
-          let ny = to[1] + dy[i];
-
-          if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-            let adj = { to: [nx, ny], cost: map[nx][ny] };
-
-            let { to: nextNode, cost: nextCost } = adj;
-
-            nextCost += cost;
-
-            // 가중치가 적거나 첫 방문일때 갱신
-            if (
-              nextCost < distance[nextNode[0]][nextNode[1]] ||
-              distance[nextNode[0]][nextNode[1]] === Infinity
-            ) {
-              distance[nextNode[0]][nextNode[1]] = nextCost;
-              pq.push({ to: nextNode, cost: nextCost });
-            }
-          }
-        }
+        // 현재 경로로 방문한 좌표가 최소비용이라면 갱신
+        if (nextValue >= dp[nx][ny]) continue;
+        dp[nx][ny] = nextValue;
+        queue.push([nx, ny]);
       }
-    };
+    }
 
-    bfs(0, 0);
-
-    return distance[N - 1][N - 1];
+    return dp[N - 1][N - 1];
   };
 
-  // 정답 출력
-  let answer = [];
-  let index = 1;
+  // 메인 로직
   while (1) {
-    if (Number(data[0]) === 0) break;
+    const N = +data[index];
+    if (N === 0) break;
+    const board = data.slice(index + 1, index + 1 + N).map((row) => row.split(' ').map(Number));
 
-    answer.push(`Problem ${index}: ${solution()}`);
+    const value = bfs(board, N);
+    result.push(`Problem ${result.length + 1}: ${value}`);
 
-    index += 1;
+    index += N + 1;
   }
-
-  console.log(answer.join('\n'));
+  console.log(result.join('\n'));
 
   process.exit();
 });
