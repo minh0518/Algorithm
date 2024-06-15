1ba8eaf930c02516fdcc53b5002ddeda3a31c0bb
minh0518 <rubics@naver.com>
2024-06-15 11:11:53 +0900

    24.6.15

--- JSstudyGroup/14503.js
+++ JSstudyGroup/14503.js
@@ -10,112 +10,65 @@ const data = [];
 rl.on('line', (input) => {
   data.push(input);
 }).on('close', () => {
-  let [N, M] = data.shift().split(' ').map(Number);
+  const [N, M] = data.shift().split(' ').map(Number);
+  let [x, y, dir] = data.shift().split(' ').map(Number);
+  const board = data.map((row) => row.split(' ').map(Number));
 
-  let [r, c, d] = data.shift().split(' ').map(Number);
-
-  let map = data.map((i) => i.split(' ').map(Number));
-
-  let visited = new Array(N).fill().map(() => new Array(M).fill(false));
-
-  let count = 0;
-
-  let [dx, dy] = [
+  // 북 동 남 서
+  const [dx, dy] = [
     [-1, 0, 1, 0],
     [0, 1, 0, -1],
   ];
 
-  let queue = [[r, c, d]];
+  // 방향은 유지한 체, 뒤로 이동
+  const goBack = (x, y, dir) => {
+    if (dir < 2) dir += 2;
+    else if (dir >= 2) dir -= 2;
+    return [x + dx[dir], y + dy[dir]];
+  };
 
-  while (queue.length) {
-    let [x, y, r] = queue.shift();
-
-
-    //해당 위치 청소
-    if (
-      x >= 0 &&
-      x < N &&
-      y >= 0 &&
-      y < M &&
-      map[x][y] === 0 &&
-      !visited[x][y]
-    ) {
+  // 메인 로직
+  let count = 0;
+  while (1) {
+    // 현재 좌표 청소
+    if (board[x][y] === 0) {
+      board[x][y] = 'x';
       count += 1;
-      visited[x][y] = true;
     }
 
+    let isMoved = false; // 주변에 빈 공간이 있는지에 대한 여부
+    let copyDir = dir; // 현재 방향 복사 (주변에 빈 공간이 있다면 이 방향을 사용)
 
-    //여기서 push되는게 없으면 while 종료
+    // 4방향 회전하면서 빈 공간 탐색
     for (let i = 0; i < 4; i++) {
-      if (r === 0) r = 3;
-      else r -= 1;
+      copyDir -= 1; // 반시계 방향으로 회전이 먼저
+      if (copyDir === -1) copyDir = 3;
 
-      let nx = x + dx[r];
-      let ny = y + dy[r];
-
-      //2-1
-      if (
-        nx >= 0 &&
-        nx < N &&
-        ny >= 0 &&
-        ny < M &&
-        map[nx][ny] === 0 &&
-        !visited[nx][ny]
-      ) {
-        queue.push([nx, ny, r]);
+      const nx = x + dx[copyDir];
+      const ny = y + dy[copyDir];
+      // 빈 공간이 있다면 copyDir 방향, 좌표대로 이동
+      if (board[nx][ny] === 0) {
+        x = nx;
+        y = ny;
+        dir = copyDir;
+        isMoved = true;
         break;
       }
-
-      //3일때가 한바퀴를 다 돌면서 갈 수 있는지 확인한 것이고
-      //위에 if문에 안 걸리게 된다면 여기서 2-3 2-4 진행
-      if (i === 3) {
-        let reverseR;
-        if (r >= 2) reverseR = r - 2;
-        else reverseR = r + 2;
-
-        let reverseX = x + dx[reverseR];
-        let reverseY = y + dy[reverseR];
-
-        //후진 가능
-        if (
-          reverseX >= 0 &&
-          reverseX < N &&
-          reverseY >= 0 &&
-          reverseY < M &&
-          map[reverseX][reverseY] === 0
-        ) {
-          queue.push([reverseX, reverseY, r]);
-          break;
-        }
-
-        //후진 불가능
-        else break
-      }
     }
-//어차피 위에 조건들에 안 걸리게 되면 2-2가 자동으로 진행(=회전만 하고 다음while문 진행)
+
+    // 빈 공간이 없다면 후진
+    if (!isMoved) {
+      const [nx, ny] = goBack(x, y, dir);
+      if (board[nx][ny] === 1) break; // while문 탈출
+      if (board[nx][ny] === 'x') {
+        x = nx;
+        y = ny;
+      }
+      continue;
+    }
   }
 
   console.log(count);
 
   process.exit();
 });
-
-
-//왼쪽 방향 이동
-//0 > -1+4 =3    북일때 서
-//1 > 0      동일때 북
-//2 > 1
-//3 > 2
-
-//뒤로가기
-//0 > 2 북일 때 남
-//1 > 3 서일 때 동
-//2 > 0 남일 때 북
-//3 > 1 동일 때 서
-
-// 1.현재 위치 청소
-// 2.현재 방향을 기준으로 왼쪽부터 탐색함
-//   1.청소 안한곳이 있으면 그 방향으로 회전후, 전진해서 청소 (다시 1번 반복)
-//   2.왼쪽청소 못하면 그 방향으로 회전만 함 (다시 2번 반복)
-//   3.네 방향 다 청소되어있고 벽이라면 방향만 유지하고 후진 (다시 2번 반복)
-//   4.네 방향 다 청소되어있고 벽이고 뒤에 벽이어서 후진도 안되면 끝
