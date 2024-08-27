dd203e07c4d9850213498325565ac8af4defae58
minh0518 <rubics@naver.com>
2024-08-27 22:41:05 +0900

    24.8.27

--- JSstudyGroup/3020.js
+++ JSstudyGroup/3020.js
@@ -1,4 +1,3 @@
-// 24.2.11
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -12,60 +11,50 @@ rl.on('line', (input) => {
   data.push(input);
 }).on('close', () => {
   const [N, H] = data.shift().split(' ').map(Number);
+  const info = data.map(Number);
 
-  const normal = [];
-  const reverse = [];
-  for (let i = 0; i < data.length; i++) {
-    if (i % 2 === 0) {
-      normal.push(+data[i]);
-    }
-    if (i % 2 === 1) {
-      reverse.push(+data[i]);
-    }
-  }
-  normal.sort((a, b) => a - b);
-  reverse.sort((a, b) => a - b);
+  const fromBottom = info.filter((_, index) => index % 2 === 0).sort((a, b) => a - b);
+  const fromTop = info.filter((_, index) => index % 2 === 1).sort((a, b) => a - b);
 
-  // 종유석,석순의 높이가 담긴 arr배열에서
-  // height이상이 값이 나오는 lowerbound탐색 후 갯수 리턴
-  const binarySearch = (height, arr) => {
+  const lowerBound = (arr, target) => {
     let left = 0;
-    let right = N / 2 - 1;
+    let right = arr.length - 1;
+
     while (left <= right) {
       const mid = Math.floor((left + right) / 2);
       const midValue = arr[mid];
 
-      if (midValue >= height) {
-        right = mid - 1;
-      }
-      if (midValue < height) {
+      if (midValue < target) {
         left = mid + 1;
       }
+      if (midValue >= target) {
+        right = mid - 1;
+      }
     }
 
-    // left가 lowerbound이므로 이를 기반으로 파괴되는 갯수를 반환
-    return N / 2 - left;
+    // lowerbound인덱스 기준으로 최종 파괴 개수 반환
+    return arr.length - left;
   };
 
-  let count = 0;
   let minValue = Infinity;
+  let count = 0;
   for (let i = 1; i <= H; i++) {
-    // 석순기준 높이
-    const normalHeight = i;
+    // 석순 파괴 개수
+    const countFromBottom = lowerBound(fromBottom, i);
+    // 종유석 파괴 개수
+    const countFromTop = lowerBound(fromTop, H - i + 1);
 
-    // 종유석 기준 높이
-    const reverseHeight = H - i + 1;
+    const total = countFromBottom + countFromTop;
 
-    // i높이로 파괴되는 갯수
-    const destoryCount = binarySearch(normalHeight, normal) + binarySearch(reverseHeight, reverse);
-
-    if (destoryCount < minValue) {
-      count = 1;
-      minValue = destoryCount;
-    } else if (destoryCount === minValue) {
+    if (total === minValue) {
       count += 1;
     }
+    if (total < minValue) {
+      minValue = total;
+      count = 1;
+    }
   }
+
   console.log(minValue, count);
 
   process.exit();
